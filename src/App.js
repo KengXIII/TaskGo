3aa920edfd336bc0d1aa6ecef5465bca774af833
KengXIII <kengi0813@gmail.com>
2021-05-29 16:44:56 +0800

    updated webpage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to our TaskGo Homepage! Stay tuned for updates~
         </p>
         <a
           className="App-link"
