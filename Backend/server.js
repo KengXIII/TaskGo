d3d54638d5f568ac715b3b4416f22da7d7592486
KengXIII <kengi0813@gmail.com>
2021-06-27 18:41:49 +0800

    empty

--- Backend/server.js
+++ Backend/server.js
@@ -22,14 +22,8 @@ app.get("/", (req, res) => {
 });
 
 app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://task-go-kengxiii.vercel.app/dashboard"
-  );
-  res.setHeader(
-    "Access-Control-Request-Method",
-    "https://task-go-kengxiii.vercel.app/dashboard"
-  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Expose-Headers", "Content-Type");
