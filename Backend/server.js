98c05d7489b2a7f88d2448838c03f103018c0896
KengXIII <kengi0813@gmail.com>
2021-06-27 17:18:06 +0800

    cors update

--- Backend/server.js
+++ Backend/server.js
@@ -11,6 +11,21 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use(express.static("public"));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Expose-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    res.writeHead(200);
+    return res.end();
+  } else {
+    return next();
+  }
+});
+
 app.post("/send_mail", cors(), async (req, res) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
--- TaskGo/Backend/Frontend/src/components/Calendar/Calendar.js
+++
@@ -1,9 +0,0 @@
-function Calendar() {
-  return (
-    <div>
-      <h2>Calendar</h2>
-    </div>
-  );
-}
-
-export default Calendar;
