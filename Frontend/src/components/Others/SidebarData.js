7ca47470988d233aaf8af86533a1860e026f57ff
KengXIII <kengi0813@gmail.com>
2021-12-27 16:22:47 +0800

    Clean up config files

--- Frontend/src/components/Others/SidebarData.js
+++ Frontend/src/components/Others/SidebarData.js
@@ -1,27 +1,23 @@
-import React from 'react';
-import DashboardIcon from '@material-ui/icons/Dashboard';
-import HistoryIcon from '@material-ui/icons/History';
-import EventNoteIcon from '@material-ui/icons/EventNote';
-import SettingsIcon from '@material-ui/icons/Settings';
+import React from "react";
+import DashboardIcon from "@material-ui/icons/Dashboard";
+import HistoryIcon from "@material-ui/icons/History";
+import EventNoteIcon from "@material-ui/icons/EventNote";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 export const SidebarData = [
-    {
-        title: "Dashboard",
-        icon: <DashboardIcon />,
-        link: "/dashboard"
-    },
-    {
-        title: "Task History",
-        icon: <HistoryIcon />,
-        link: "/task-history",
-    },
-    {
-        title: "Calendar",
-        icon: <EventNoteIcon />,
-        link: "/calendar"
-    },{
-        title: "Settings",
-        icon: <SettingsIcon />,
-        link: "/settings"
-    },
+  {
+    title: "Dashboard",
+    icon: <DashboardIcon />,
+    link: "/dashboard",
+  },
+  {
+    title: "Task History",
+    icon: <HistoryIcon />,
+    link: "/task-history",
+  },
+  {
+    title: "Settings",
+    icon: <SettingsIcon />,
+    link: "/settings",
+  },
 ];
